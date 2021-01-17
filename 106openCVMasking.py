f043407dd6c2184523a368bd01ae3b234ecc8561
kelvin926 <kelvin926@naver.com>
2021-01-18 07:41:52 +0900

    7시40분 커밋

--- 106openCVMasking.py
+++ 106openCVMasking.py
@@ -14,13 +14,14 @@ center = (width // 2, height // 2)
  
 cv2.imshow("nomadProgramer", img)
 
-mask = np.zeros(img.shape[:2], dtype = "uint8")
+mask = np.zeros(img.shape[:2], dtype = "uint8")  # 제로 필 된 넘파이 행렬.
 
-cv2.circle(mask, center, 300, (255, 255, 255), -1)
+cv2.circle(mask, center, 300, (255, 255, 255), -1) # 마스크에다가 원으로 된 가득 찬 흰색 마스크 생성. (검은색 제로필에 흰색 원 생성.)
 
 cv2.imshow("mask", mask)
  
-masked = cv2.bitwise_and(img, img, mask = mask)
+masked = cv2.bitwise_and(img, img, mask = mask) # and 연산자. 합친다!! / 검은색 부분은 false값 이므로, and 연산에서 제외. so, 둘 다 true (흰색, 그림) 부분만 보이게 됨.
+# or이라도, 마스크가 가려버리는 듯.
 cv2.imshow("nomadProgramer with mask", masked)
 
 cv2.waitKey(0)
--- 107openCVChannel.py
+++ 107openCVChannel.py
@@ -14,28 +14,28 @@ center = (width // 2, height // 2)
 
 cv2.imshow("nomadProgramer", img)
 
-(Blue, Green, Red) = cv2.split(img)
+(Blue, Green, Red) = cv2.split(img) # BGR로 색 분리. 스플린트 한 다음에 BGR로 들어감.
 
-cv2.imshow("Red Channel", Red)
-cv2.imshow("Green Channel", Green)
-cv2.imshow("Blue Channel", Blue)
+cv2.imshow("Red Channel", Red) # 레드 영역만.
+cv2.imshow("Green Channel", Green) # 그린 영역만.
+cv2.imshow("Blue Channel", Blue) # 블루 영역만.
 cv2.waitKey(0)
 
-zeros = np.zeros(img.shape[:2], dtype = "uint8")
-cv2.imshow("Red", cv2.merge([zeros, zeros, Red]))
-cv2.imshow("Green", cv2.merge([zeros, Green, zeros]))
-cv2.imshow("Blue", cv2.merge([Blue, zeros, zeros]))
+zeros = np.zeros(img.shape[:2], dtype = "uint8") # 제로필.
+cv2.imshow("Red", cv2.merge([zeros, zeros, Red])) # 레드 필터
+cv2.imshow("Green", cv2.merge([zeros, Green, zeros])) # 그린 필터
+cv2.imshow("Blue", cv2.merge([Blue, zeros, zeros])) # 블루 필터
 cv2.waitKey(0)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # 그레이 스케일
 cv2.imshow("Gray Filter", gray)
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # HSV 필터
 cv2.imshow("HSV Filter", hsv)
-lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB) # LAB 필터
 cv2.imshow("LAB Filter", lab)
 cv2.waitKey(0)
 
-BGR = cv2.merge([Blue, Green, Red])
+BGR = cv2.merge([Blue, Green, Red]) # 색 다시 합쳐서 출력.
 cv2.imshow("Blue, Green and Red", BGR)
 
 cv2.waitKey(0)
